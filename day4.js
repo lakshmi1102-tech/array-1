b42850d93b4bc1d6993cc514f01834fb63d338dd
lakshmi1102-tech <konankilakshmi0211@gmail.com>
2024-12-07 15:59:52 +0530

    array methods

---
+++ day4.js
@@ -0,0 +1,76 @@
+// var arr=["apple","banana","cutsord apple","dragon","grapes"]
+// console.log(arr.length) 
+//
+// var arr=["apple","banana","cutsord apple","dragon","grapes"]
+//   arr.push("hello")
+//   console.log(arr)
+
+// var arr3=["apple","banana","cutsord apple","dragon","grapes"]
+//   arr3.pop("grapes")
+//   console.log(arr3)
+
+// var arr4=["apple","banana","cutsord apple","dragon","grapes"]
+//   arr4.unshift("dates")
+//   console.log(arr4)
+//   
+// var arr5=["apple","banana","cutsord apple","dragon","grapes"]
+//     arr5.shift("apple")
+//     console.log(arr5)
+
+// var arr51=["apple","banana","cutsord apple","dragon","grapes"];
+// console.log(arr51.reverse());
+
+// var arr6=["apple","banana","cutsord apple","dragon","grapes"]
+//   arr6.indexOf("banana")
+//   console.log(arr6)
+
+// var arr7=["apple","banana","cutsord apple","dragon","grapes"]
+//   arr7.includes("Apple")
+//   console.log(arr7)
+
+// var arr8=["apple","banana","cutsord apple","dragon","grapes"]
+// console.log(arr8.sort())
+
+// var arr9=["apple","banana","custord"]
+//  console.log(arr9.join(" "));
+
+// var arr21=["apple","banana","cutsord apple","dragon","grapes"]
+// console.log(arr21.push("ice").shift("apple"))
+
+// var arr22=["apple","banana","cutsord apple","dragon","grapes"]
+// console.log(arr22.reverse().join())
+
+// var arr23=["apple","banana","cutsord apple","dragon","grapes"]
+// console.log(arr23.sort().pop("grapes"))
+// var arr24=["apple","banana","cutsord apple","dragon","grapes"]
+//     arr24.unshift("guvva","orange")
+//     console.log(arr24.shift("guvva").length)
+
+// var arr25=["apple","banana","cutsord apple","dragon","grapes"]
+// var arr26=["beens","chachocos","butter"]
+//      console.log(arr25.concat(arr26).pop("butter"))
+// var arr31=["Alice","Bob","Charlie","David"]
+// arr31.unshift("hello")
+// arr31.pop("David")
+// arr31.includes("Bob")
+// console.log(arr31)
+// var a="learn ,practice, improve"
+//     a.spilt("")
+//     a.reverse().join(" ")
+//     console.log(a)
+// var a1=["song1","song2","song3"]
+// var a2=["song4","song5"]
+//   a1.concat(a2)
+//   a1.shift("song1")
+//   a1.push("song6")
+//   console.log(a1)
+
+// var b=["inception","titanic","avatar","interseller","gladiator"]
+//    b1=b.slice(1,4)
+// var c=["the matrix","the godfather"]
+//   combined=b1.concat(c)
+//   console.log(combined.reverse())
+// var d="john Doe jane smith"
+//   d.split("")
+//   d.includes("jane").reverse().join(" ")
+//   console.log(d)
